e1559e924975a48882de4c18d2132a411915e905
daisukiyo <tk.rain09@gmail.com>
2019-03-18 19:47:42 -0700

    add react router to main component

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,8 +1,67 @@
 import React, { Component } from 'react';
+import Landing from '../landing/Landing'
 import './Main.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function LandingPage() {
+    
+}
+
+function TerminalPage() {
+
+}
+
+function EditorPage() {
+
+}
+
+function LanguagePage() {
+
+}
+
+function ToolPage() {
+
+}
+
+function DownloadPage() {
+
+}
 
 const Main = (props) => (
-    <div className="main"></div>
+    <Router>
+        <div className="tempNav">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to = "/">Landing</Link>
+                    </li>
+                    <li>
+                        <Link to = "/form/1">Terminal</Link>
+                    </li>
+                    <li>
+                        <Link to = "/form/2">Editor</Link>
+                    </li>
+                    <li>
+                        <Link to = "/form/3">Language</Link>
+                    </li>
+                    <li>
+                        <Link to = "/form/4">Tool</Link>
+                    </li>
+                    <li>
+                        <Link to = "/form/download">Download</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            
+
+        </div>
+        <div className="main">
+            <Route path="/" exact component={Landing} />
+            {/* <Route path="/form" component={} /> */}
+        </div>
+    </Router>
+    
 )
 
 export default Main
