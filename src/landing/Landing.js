3ad9611a529daa4cb650a08c16fcfc2aba85e448
daisukiyo <tk.rain09@gmail.com>
2019-03-19 16:45:53 -0700

    add button and title to landing view

--- src/landing/Landing.js
+++ src/landing/Landing.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './Landing.css'
 import logo from '../logo.png';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-const Landing = (props) => (
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+    input: {
+      display: 'none',
+    },
+  });
+
+function Landing(props) {
+    const { classes } = props;
+    return (
     <div className="Landing">
         <img id="logo" src={logo} className="App-logo" alt="logo" />
-        <p>Quicky</p>
+        <h1>Quicky</h1>
+        <h2>The one stop shop for your dev environment</h2>
+        <Button variant="contained" className={classes.button}>
+        Get Started
+        </Button>
     </div>
+    )
+}
 
-)
+Landing.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
 
-export default Landing
+export default withStyles(styles)(Landing)
