720189f4de7575616fc67c304f02b1a38d4d1cd0
daisukiyo <tk.rain09@gmail.com>
2019-03-20 12:07:09 -0400

    add material-ui stepper

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import Landing from '../landing/Landing'
 import './Main.css'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// Horizontal Linear Stepper
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepButton from '@material-ui/core/StepButton';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 function LandingPage() {
     
@@ -27,41 +35,175 @@ function DownloadPage() {
 
 }
 
-const Main = (props) => (
-    <Router>
-        <div className="tempNav">
-            <nav>
-                <ul>
-                    <li>
-                        <Link to = "/">Landing</Link>
-                    </li>
-                    <li>
-                        <Link to = "/form/1">Terminal</Link>
-                    </li>
-                    <li>
-                        <Link to = "/form/2">Editor</Link>
-                    </li>
-                    <li>
-                        <Link to = "/form/3">Language</Link>
-                    </li>
-                    <li>
-                        <Link to = "/form/4">Tool</Link>
-                    </li>
-                    <li>
-                        <Link to = "/form/download">Download</Link>
-                    </li>
-                </ul>
-            </nav>
+// Stepper Styling
+const styles = theme => ({
+  root: {
+    width: '95%',
+  },
+  button: {
+    marginRight: theme.spacing.unit,
+  },
+  completed: {
+    display: 'inline-block',
+  },
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  },
+});
 
-            
+function getSteps() {
+    return ['Terminal/Shell', 'IDE/Editor', 'Language', 'Dev Tools'];
+}
 
-        </div>
+function getStepContent(step) {
+    switch (step) {
+        case 0:
+            return 'Terminal/Shell';
+        case 1:
+            return 'IDE/Editor';
+        case 2:
+            return 'Language';
+        case 3  :
+            return 'Dev Tools';
+    }
+}
+
+class Main extends React.Component {
+  state = {
+    activeStep: 0,
+    completed: {},
+  }; 
+
+  totalSteps = () => getSteps().length;
+
+  handleNext = () => {
+    let activeStep;
+
+    if (this.isLastStep() && !this.allStepsCompleted()) {
+      // It's the last step, but not all steps have been completed,
+      // find the first step that has been completed
+      const steps = getSteps();
+      activeStep = steps.findIndex((step, i) => !(i in this.state.completed));
+    } else {
+      activeStep = this.state.activeStep + 1;
+    }
+    this.setState({
+      activeStep,
+    });
+  };
+
+  handleBack = () => {
+    this.setState(state => ({
+      activeStep: state.activeStep - 1,
+    }));
+  };
+
+  handleStep = step => () => {
+    this.setState({
+      activeStep: step,
+    });
+  };
+
+  handleComplete = () => {
+    const { completed } = this.state;
+    completed[this.state.activeStep] = true;
+    this.setState({
+      completed,
+    });
+    this.handleNext();
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0,
+      completed: {},
+    });
+  };
+
+  completedSteps() {
+    return Object.keys(this.state.completed).length;
+  }
+
+  isLastStep() {
+    return this.state.activeStep === this.totalSteps() - 1;
+  }
+
+  allStepsCompleted() {
+    return this.completedSteps() === this.totalSteps();
+  }
+  
+    render() {
+      const { classes } = this.props;
+      const steps = getSteps();
+      const { activeStep } = this.state;
+  
+      return (
+        <Router>
         <div className="main">
-            <Route path="/" exact component={Landing} />
-            {/* <Route path="/form" component={} /> */}
+        <Route path="/" exact component={Landing} />
+`
+        <div className={classes.root}>
+        <Stepper nonLinear activeStep={activeStep}>
+          {steps.map((label, index) => (
+            <Step key={label}>
+              <StepButton onClick={this.handleStep(index)} completed={this.state.completed[index]}>
+                {label}
+              </StepButton>
+            </Step>
+          ))}
+        </Stepper>
+        <div>
+          {this.allStepsCompleted() ? (
+            <div>
+              <Typography className={classes.instructions}>
+                All steps completed - you&apos;re finished
+              </Typography>
+              <Button onClick={this.handleReset}>Reset</Button>
+            </div>
+          ) : (
+            <div>
+              <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+              <div>
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={this.handleBack}
+                  className={classes.button}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.handleNext}
+                  className={classes.button}
+                >
+                  Next
+                </Button>
+                {activeStep !== steps.length &&
+                  (this.state.completed[this.state.activeStep] ? (
+                    <Typography variant="caption" className={classes.completed}>
+                      Step {activeStep + 1} already completed
+                    </Typography>
+                  ) : (
+                    <Button variant="contained" color="primary" onClick={this.handleComplete}>
+                      {this.completedSteps() === this.totalSteps() - 1 ? 'Finish' : 'Complete Step'}
+                    </Button>
+                  ))}
+              </div>
+            </div>
+          )}
         </div>
-    </Router>
-    
-)
+      </div>
+        </div>
+        </Router>
+      );
+    }
+  }
 
-export default Main
+Main.propTypes = {
+    classes: PropTypes.object,
+  };
+  
+
+export default withStyles(styles)(Main);
