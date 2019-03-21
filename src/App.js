f34c1326e91659ef71c09c076cca17767b866c84
Connor Cahill <connorcahill913@gmail.com>
2019-03-20 23:38:23 -0700

    [Adds]: routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Main from './main/Main';
+import Landing from './landing/Landing';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main></Main>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route
+            path="/"
+            exact
+            render={() => <Landing /> }
+          />
+
+          <Route
+            path="/form/:step"
+            exact
+            render={({ match }) => {
+              // step from url param 
+              const { step } = match.params;
+              return (
+                <Main step={step} /> 
+              )
+            }}
+          />
+
+        </div>
+      </BrowserRouter>
+      
     );
   }
 }
