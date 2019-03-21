cadf3811b0694f0fc8d5996b85c59870e900388d
Connor Cahill <connorcahill913@gmail.com>
2019-03-21 00:09:02 -0700

    [Refactors]: form component to generate radio buttons through obj

--- src/form/Form.js
+++ src/form/Form.js
@@ -13,13 +13,27 @@ const styles = theme => ({
         display: 'none',
     },
 });
+
+// should probably put this is state.
+// will later be handled with backend request
+// object that holds info for checkboxes (not all correct right now)
+const installOptions = {
+    'editors': ['vim', 'vscode', 'emacs'],
+    'shells': ['zsh', 'fish'],
+    'languages': ['python', 'node', 'go'],
+};
  
 function Form(props) {
-    const { classes } = props;
+    const { classes, step } = props;
     return (
         <div className='form-page'>
             <h1>Title</h1>
             <h3>Description goes here...</h3>
+            {
+                step === 1 // if form is step 1
+                ? installOptions.shells.map(option => <input type="radio" name="Shell" value="zsh"></input>{ option }<br/> )
+                : null
+            }
             <div className = "options">
                 {/* This is just place holder for styling/proof of concept */}
                 <input type="radio" name="Shell" value="Zsh"></input>Zsh<br></br>
