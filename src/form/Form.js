bcaa37023c7ae2a95c457abaf0e9d663dd1068b4
Connor Cahill <connorcahill913@gmail.com>
2019-03-22 03:43:50 -0700

    [Adds Feat]: can now add items from form inputs to itemsObj. ALSO formats file

--- src/form/Form.js
+++ src/form/Form.js
@@ -3,6 +3,7 @@ import './Form.css';
 import { withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 //import Button from '@material-ui/core/Button';
+import { itemsAdd } from '../actions/items';
 
 
 const styles = theme => ({
@@ -14,18 +15,19 @@ const styles = theme => ({
     },
 });
 
+
 function Form(props) {
     // eslint-disable-next-line
-    const { itemsObj, classes, step } = props;
+    const { itemsObj, classes, step, items } = props;
     return (
         <div className='form-page'>
             <h1>{itemsObj.currentCategory}</h1>
             <h3>{itemsObj.currentDesc}</h3>
-            <div className = "options">
+            <div className="options">
                 {
                     itemsObj.currentCategory !== 'Shells'
-                        ? itemsObj.currentItems.map(opt => <div><input type="checkbox" name={itemsObj.currentCategory} value={ opt }></input>{ opt }<br /></div>)
-                        : itemsObj.currentItems.map(opt => <div><input type="radio" name={itemsObj.currentCategory} value={ opt }></input>{ opt }<br /></div>)
+                        ? itemsObj.currentItems.map(opt => <div key={opt}><input type="checkbox" name={itemsObj.currentCategory} value={opt} onClick={() => props.dispatch(itemsAdd(props.items, itemsObj.currentCategory, opt))}></input>{opt}<br /></div>)
+                        : itemsObj.currentItems.map(opt => <div key={opt}><input type="radio" name={itemsObj.currentCategory} value={opt} onClick={() => props.dispatch(itemsAdd(props.items, itemsObj.currentCategory, opt))}></input>{opt}<br /></div>)
                 }
             </div>
         </div>
