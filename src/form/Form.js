1798b1c2b5989c437d1b967018d728241deba7ff
Connor Cahill <connorcahill913@gmail.com>
2019-03-22 19:26:55 -0700

    [Fixes #4]: checkmarks are now persistent

--- src/form/Form.js
+++ src/form/Form.js
@@ -21,38 +21,64 @@ class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected: "",
+            selectedShell: "",
+            selected: {},
         };
 
         this.toggleActions = this.toggleActions.bind(this);
         this.handleSelect = this.handleSelect.bind(this);
     }
-
-    toggleActions = (e, checked) => {
+    toggleActions = (item, checked) => {
         const { itemsObj } = this.props;
         // checking if box is checked
         if (checked) {
             // if checked running our action itemsAdd
-            this.props.dispatch(itemsAdd(this.props.items, itemsObj.currentCategory, e.target.value));
+            this.props.dispatch(itemsAdd(this.props.items, itemsObj.currentCategory, item.value));
         } else {
             // else, (uncheck) calling our action itemsRemove
-            this.props.dispatch(itemsRemove(this.props.items, itemsObj.currentCategory, e.target.value));
+            this.props.dispatch(itemsRemove(this.props.items, itemsObj.currentCategory, item.value));
         }
     };
     // makes so users can only check one option when choosing a shell
-    handleSelect = (e) => {
+    handleSelectShell = (e) => {
         const { itemsObj } = this.props;
         this.props.dispatch(itemsRemove(this.props.items, itemsObj.currentCategory, this.state.selected));
         return this.setState({
-            selected: e.target.id,
+            selectedShell: e.target.id,
         });
     };
-
+    // handles what happens when an option is selected
+    // this method helps us persist the checked checkboxes as well
+    handleSelect = async (item) => {
+        try {
+            const { selected } = this.state;
+            const newSelected = selected;
+            // item already selected and being unchecked
+            if (selected[item.value]) {
+                // already check, set false now
+                newSelected[item.value] = false;
+            } else {
+                // not checked, set as true
+                newSelected[item.value] = true;
+            }
+           // push the new selected option into state
+            await this.setState({
+                selected: newSelected,
+            });
+            // call our toggleActions function
+            this.toggleActions(item, selected[item.value])
+    
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.log('Error selecting this options: ', err);
+        };
+   }
 
     render() {
          // eslint-disable-next-line
         const { itemsObj, classes, step, items } = this.props;
-       
+        
+
     return (
         <div className='form-page'>
             <h1>{itemsObj.currentCategory}</h1>
@@ -60,8 +86,8 @@ class Form extends Component {
             <div className="options">
                 {
                     itemsObj.currentCategory !== 'Shells'
-                        ? itemsObj.currentItems.map(opt => <div key={opt}><Checkbox name={itemsObj.currentCategory} value={opt} onChange={this.toggleActions}></Checkbox>{opt}<br /></div>)
-                        : itemsObj.currentItems.map(opt => <div key={opt}><Checkbox id={ opt } name={itemsObj.currentCategory} checked={this.state.selected === opt} value={opt} onClick={this.handleSelect} onChange={this.toggleActions}></Checkbox>{opt}<br /></div>)
+                        ? itemsObj.currentItems.map(opt => <div key={opt}><Checkbox name={itemsObj.currentCategory} value={opt} checked={this.state.selected[opt] === true} onChange={(e) => this.handleSelect(e.target)}></Checkbox>{opt}<br /></div>)
+                        : itemsObj.currentItems.map(opt => <div key={opt}><Checkbox id={ opt } name={itemsObj.currentCategory} checked={this.state.selectedShell === opt} value={opt} onClick={this.handleSelectShell} onChange={this.toggleActions}></Checkbox>{opt}<br /></div>)
                 }
             </div>
         </div>
