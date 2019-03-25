7be473205d3d3c9e59c8dbc3704edaa34e834e45
C3NZ <cenz@cenz.io>
2019-03-25 09:39:46 -0700

    [fix] update the setup page

--- src/download/Download.js
+++ src/download/Download.js
@@ -28,16 +28,20 @@ function Download(props) {
                 <code>
                     // Installation Instructions
                     <br /><br />
-                    1. Copy setup script
-                    <br />
-                    2. Open your terminal
-                    <br />
-                    3. Paste setup script into your terminal
-                    <br />
-                    5. Press enter key
-                    <br />
-                    4. Dance around and be happy
-                <FilledInput underline style={{ width: '75%' }} value={`bash $(curl -fsSL http://quicky.dev/${sessionStorage.getItem('filePath')})`} />
+                    1. Copy the command at the bottom
+                    <br /><br />
+                    2. Open your terminal, paste in the command, and run.
+                    <br /><br />
+                    3. If you have xcode, you will be prompted to install the xcode CLI tools. This will open an xcode 
+                    <br />GUI that you should wait for to finish all while the next stage is prepped. 
+                    <br /><br />
+                    4. When the GUI installer is finished, press enter in the terminal to resume the installation
+                    <br /><br />
+                    5. Dance around and be happy! You may be prompted for a password a 
+                    <br />couple of times depending on what it is you chose to install, but other than that there is no
+                    <br />nothing else required on your part. You can run apps as soon as they're installed
+                    <br /><br />
+                    <FilledInput underline style={{ width: '75%' }} value={`bash <(curl -fsSL https://quicky-api.herokuapp.com/api/scripts/${sessionStorage.getItem('filePath')})`} />
                     <br /><br />
                 </code>
             </pre>
