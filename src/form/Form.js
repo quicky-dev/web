2f2348be7763ae0d7d2b6a7652b07f835cbd39b7
Connor Cahill <connorcahill913@gmail.com>
2019-03-25 11:54:26 -0700

    [Updates]: default shell selection no longer needed.

--- src/form/Form.js
+++ src/form/Form.js
@@ -21,7 +21,7 @@ class Form extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedShell: "fish",
+            selectedShell: "",
             selected: {},
         };
 
