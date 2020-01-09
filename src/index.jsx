fb8181bd77b9ecc93026bc31145f969b4a23e1c0
C3NZ <cenz@cenz.io>
2020-01-08 20:44:30 -0800

    [fix] filenames, add eslint and husky for pre commit linting, and configure eslint for jest

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import App from './app';
+import * as serviceWorker from './serviceWorker';
 
-import reducers from "./redux/reducers/index";
+import reducers from './redux/reducers/index';
 
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-
-import thunk from "redux-thunk";
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
@@ -17,7 +16,7 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import itemsReducer from './itemsReducer';
 
 export default combineReducers({
-    items: itemsReducer,
-})
+  items: itemsReducer,
+});
