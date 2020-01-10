589d6d6e2e2b0c5f5781746377cebb7a0e777b3c
Timothy Kaing <kaing.timothy@gmail.com>
2020-01-10 09:37:40 -0800

    redesigned download page

--- src/pages/download/Download.jsx
+++ src/pages/download/Download.jsx
@@ -1,36 +1,74 @@
 import React, { PureComponent } from 'react';
-import './Download.css';
+import styled from 'styled-components';
 import { withStyles } from '@material-ui/core/styles';
-import { FilledInput } from '@material-ui/core';
+import Clipboard from 'react-clipboard.js';
 
-const styles = (theme) => ({
-  button: {
-    margin: theme.spacing.unit,
-  },
-  input: {
-    display: 'none',
-  },
-});
+const FormContainer = styled.div`
+  background-color: #fff;
+  min-width: 70vw;
+  min-height: 70vh;
+  padding: 4em;
+  border-radius: 20px;
+    -webkit-box-shadow: 0 14px 28px rgba(0,0,0,0.15), 0 10px 10px rgba(0,0,0,0.11);
+    -moz-box-shadow: 0 14px 28px rgba(0,0,0,0.15), 0 10px 10px rgba(0,0,0,0.11);
+    box-shadow: 0 14px 28px rgba(0,0,0,0.15), 0 10px 10px rgba(0,0,0,0.11);
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const DownloadPage = styled.div`
+  background-color: #D3D3D3;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Heading = styled.h1`
+  font-size: 6em;
+  margin: .15em 0;
+`;
+
+const Subheading = styled.h3`
+  font-size: 1.5em;
+  margin-bottom: 2em;
+`;
+
+const CopySection = styled.div`
+`;
+
+const Script = styled.input`
+  width: 50%;
+`;
+
+const CopyBtn = styled(Clipboard)`
+  
+`;
 
 class Download extends PureComponent {
   render() {
     const curlCmd = 'bash <curl -s https://api.quicky.dev/api/scripts/';
     const scriptLocation = `${sessionStorage.getItem('filePath')}`;
     return (
-      <div className="dl-page">
-        <div className="Instructions">
-          <h1>Well, that was quick!</h1>
-          <p>
+      <DownloadPage>
+        <FormContainer>
+          <Heading>Well, that was quick!</Heading>
+          <Subheading>
             Copy and run the following bash script to set up your dev
             environment
-          </p>
-          <code>
-            <FilledInput underline value={curlCmd + scriptLocation} />
-          </code>
-        </div>
-      </div>
+          </Subheading>
+          <CopySection>
+            <Script value={curlCmd + scriptLocation} />
+            <CopyBtn data-clipboard-text={curlCmd + scriptLocation}>
+              copy
+            </CopyBtn>
+          </CopySection>
+        </FormContainer>
+      </DownloadPage>
     );
   }
 }
 
-export default withStyles(styles)(Download);
+export default Download;
